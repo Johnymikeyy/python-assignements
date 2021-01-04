38030ef843d3536143f852bb5f4dfc04f1d69a03
John <johnymikeyy@gmail.com>
2021-01-05 00:09:38 +0100

    correction

--- assignement7.py
+++ assignement7.py
@@ -3,4 +3,8 @@ numbers = [1, 3, 7, 4, 3, 0, 3, 6, 3]
 print("Given List:",numbers)
 repeat = max(set(numbers), key = numbers.count)
 
+print(repeat)
+
+print(numbers.count(3))
+
 print("the most frequent number is", int(repeat), "and", "it was repeated", numbers.count(3), "times")
