1fb483d8c43070363fefc1b7f774cd26a6d9dad6
John <johnymikeyy@gmail.com>
2021-01-11 00:31:48 +0100

    go on

--- workingplace.py
+++ workingplace.py
@@ -1,8 +1,25 @@
-number = 9
+flowers = ['Rose', 'Orchid', 'Tulip']
+count1 = len(flowers)
+count2 = 0
 
-if number >= 10:
-    print("The number is equal or greater than 10")
+while count1>0 :
+    print(flowers[count2])
+    count1 -= 1
+    count2 += 1
 
-else:
-    print("The number is less than 10") 
+for i in {'n1' : 'one', 'n2' : 'two'} : print(i) 
+
+
+iterable = [1, 2, 3, 4]
+
+for i in iterable:
+    print(i**2)
+
+n = int(input('enter a number between 1-10'))
+
+for i in range(11):
+    print('{}x{} = '.format(n, i), n*i)
+    
+print(18**2)
+
 
