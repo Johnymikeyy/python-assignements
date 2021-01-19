3c63dd99b0fd8253be08d19bfcc1c5013e6684e8
John <johnymikeyy@gmail.com>
2021-01-20 00:36:57 +0100

    assignement

---
+++ 007/3(Armstrong).py
@@ -0,0 +1,23 @@
+num = int(input("Enter a number: "))
+
+length = len(str(num))
+
+
+total = 0
+new_num = num
+
+while new_num > 0:
+    digit = new_num % 10
+    total += digit ** length
+    new_num //= 10
+ 
+if num == total:
+    print(num,"is an Armstrong number")
+
+else:
+    print(num,"is not an Armstrong number")
+    
+
+   
+   
+ 
