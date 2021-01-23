ba409eda610c7388747e38f30abb689482594fe0
John <johnymikeyy@gmail.com>
2021-01-23 14:32:15 +0100

    armstrong

--- 007/3(Armstrong).py
+++ 007/3(Armstrong).py
@@ -1,21 +1,21 @@
-num = int(input("Enter a number: "))
+num = input("Enter a number: ")
 
-length = len(str(num))
+list_num = list(num)
+length = len(num)
 
 
 total = 0
-new_num = num
 
-while new_num > 0:
-    digit = new_num % 10
-    total += digit ** length
-    new_num //= 10
- 
-if num == total:
-    print(num,"is an Armstrong number")
-    
+if not num.isdigit():
+    print("It is an invalid entry. Don't use non-numeric, float, or negative values!")
+  
 else:
-    print(num,"is not an Armstrong number")
+    for i in list_num:
+        total += int(i) ** length
+    if int(num) == total:
+        print(num,"is an Armstrong number")    
+    else:
+        print(num,"is not an Armstrong number")
 
 
    
