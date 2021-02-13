67feef154a3c9ded4bce7f23cf84612aa5563478
fgok <fgokdata@gmail.com>
2021-02-13 15:32:03 +0100

    some works

--- 007/bos.py
+++ 007/bos.py
@@ -4,8 +4,13 @@ for i in range(2, 101):
         print(j)
  
  
-year = int(input("Please enter a year: "))
-if not year % 400 or (not year % 4 and year % 100):
+year = int(input("Enter a year: "))
+if year % 4 == 0:
+  if year % 400 == 0:
+    print(f"{year} is a leap year")
+  elif year % 100 == 0:
+    print(f"{year} is a not leap year")
+  else:
     print(f"{year} is a leap year")
 else:
-    print(f"{year} is not a leap year") 
+  print(f"{year} is a not leap year")
