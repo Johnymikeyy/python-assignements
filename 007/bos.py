d30c8628d48c77ec0d983ba4faebb7b95ffd2574
fgok <fgokdata@gmail.com>
2021-02-13 15:28:00 +0100

    change of leap year

--- 007/bos.py
+++ 007/bos.py
@@ -3,3 +3,9 @@ for i in range(2, 101):
     for j in range(2,i):
         print(j)
  
+ 
+year = int(input("Please enter a year: "))
+if not year % 400 or (not year % 4 and year % 100):
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year") 
